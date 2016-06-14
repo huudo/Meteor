185b8a3c1bb5edbaa461c7ca0ad7d1f74bcbc911
Trong Huu <huudt.3012@gmail.com>
2016-06-14 11:41:43 +0700

    6/14

--- client/teamplate/news/news_page.js
+++ client/teamplate/news/news_page.js
@@ -6,22 +6,3 @@ Template.newsPage.helpers({
     return Meteor.userId();
   },
 });
-Template.newsPage.events({
-  'submit form': function(e) {
-    e.preventDefault();
-    var currentPostId = this._id;
-
-    var postProperties = {
-      body: $(e.target).find('[name=comment_text]').val()
-     
-    }
-
-    Meteor.call('commentInsert', postProperties, function(error, result) {
-      // display the error to the user and abort
-      if (error)
-        return alert(error.reason);
-     
-      Router.go('newsPage', {_id: currentPostId});  
-    });
-  }
-});
--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,11 @@
 Meteor.publish('news', function() {
   return News.find();
 });
-Meteor.publish('comments', function() {
+Meteor.publish('comments', function(postId) {
+  
   return Comments.find();
+});
+
+Meteor.publish('notifications', function() {
+  return Notifications.find({userId: this.userId, read: false});
 });
